c340d98a6df3998d4f8a689d26658a651cfa038b
Ben Stewart <ben.stewart@redbrickai.com>
2025-03-21 15:29:39 +0200

    reset code

--- myApp.js
+++ myApp.js
@@ -3,30 +3,21 @@ const helmet = require('helmet');
 const app = express();
 
 
-
-
-
-
-
 // Use Helmet for security
 app.use(helmet()); // This applies Helmet as middleware
-
-//
 app.use(helmet.hidePoweredBy());
 
 
-// Other routes and middlewares
+
+// DO NOT TOUCH BEYOND HERE
+module.exports = app;
+const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-
-const api = require('./server.js');
 app.use('/_api', api);
-
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-// Listen on port
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
