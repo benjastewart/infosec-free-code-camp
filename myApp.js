239b2e2ec22b1a5ed22b656a83237469b404265c
Ben Stewart <ben.stewart@redbrickai.com>
2025-03-21 14:52:20 +0200

    add initial document

--- myApp.js
+++ myApp.js
@@ -1,60 +1,23 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 
+// Use Helmet for security
+app.use(helmet()); // This applies Helmet as middleware
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
+// Other routes and middlewares
 app.use(express.static('public'));
 app.disable('strict-transport-security');
+
+const api = require('./server.js');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+// Listen on port
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
